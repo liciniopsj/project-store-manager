57694882a303fbba57da299c3ac106296f605263
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2023-03-22 00:06:31 -0300

    Added product router to /products route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const productRouter = require('./router/products.router');
 
 const app = express();
 app.use(express.json());
+
+app.use('/products', productRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {

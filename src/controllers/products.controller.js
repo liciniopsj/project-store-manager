91acb613aa3fdd07a85a353efc2cc53ccac0d038
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2023-03-22 00:21:03 -0300

    feat: returnAllProducts function to product controller, no error treatment

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,1 +1,9 @@
 const productsService = require('../services/products.service');
+
+const returnAllProducts = async (_req, res) => {
+  const { type, message } = await productsService.getAllProducts();
+
+  // use type to return error type.
+
+  return res.status(200).json(message);
+};

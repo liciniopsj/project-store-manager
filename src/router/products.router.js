4eda05686181148d3b2317ac138b11201e1851ba
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2023-03-22 00:05:42 -0300

    product route Router mocked setup

---
+++ src/router/products.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const productRouter = express.Router();
+
+// productRouter
+//   .get('/', 'selectAll controller')
+//   .get('/:id', 'selectById controller');
+
+module.exports = productRouter;

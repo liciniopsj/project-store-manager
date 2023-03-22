503cf76b428e1f936810407b4dbb9bb095dedfef
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2023-03-22 00:12:44 -0300

    Added products service

---
+++ src/services/products.service.js
@@ -0,0 +1,7 @@
+const productsModel = require('../models/products.model');
+
+const getAllProducts = async () => {
+  const data = await productsModel.selectAll();
+
+  return { type: null, message: data };
+};
